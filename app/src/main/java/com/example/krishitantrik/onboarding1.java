fa9f8842e6a2a9cafea9b9c8290e60b58043e927
lonewolf2299 <priyanshukatyayni229@gmail.com>
2023-11-05 21:12:56 +0530

    home and splash screen

--- app/src/main/java/com/example/krishitantrik/onboarding1.java
+++ app/src/main/java/com/example/krishitantrik/onboarding1.java
@@ -2,13 +2,26 @@ package com.example.krishitantrik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class onboarding1 extends AppCompatActivity {
+    LinearLayout forward_onboardingLL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding1);
+
+        forward_onboardingLL = findViewById(R.id.forward_onboardingLL);
+        forward_onboardingLL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), onboarding2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/krishitantrik/onboarding2.java
+++ app/src/main/java/com/example/krishitantrik/onboarding2.java
@@ -2,13 +2,25 @@ package com.example.krishitantrik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class onboarding2 extends AppCompatActivity {
+    private Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding2);
+        signUpButton = findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
